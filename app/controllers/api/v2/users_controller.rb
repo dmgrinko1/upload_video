ec0cfe35aa14005df2b933441ed4baf7b9b16ca8
dmgrinko1@gmail.com <dmgrinko1@gmail.com>
2020-01-18 10:09:15 +0200

    #add support v2 api versions

---
+++ app/controllers/api/v2/users_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Api::V2::UsersController < BaseApiController
+  def list_of_requests; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,26 @@ Rails.application.routes.draw do
       end
       resources :videos, only: [:index, :show] do
         member do
-          post 'retry', to: 'videos#retry', as: 'retry'
+          post 'failed/retry', to: 'videos#retry', as: 'retry'
         end
+
+        collection do
+          post 'upload'
+        end
+      end
+    end
+
+    namespace 'v2' do
+      resources :users, only: [] do
+        collection do
+          get 'list_of_requests'
+        end
+      end
+      resources :videos, only: [:index, :show] do
+        member do
+          post 'failed/retry', to: 'videos#retry', as: 'retry'
+        end
+
         collection do
           post 'upload'
         end
