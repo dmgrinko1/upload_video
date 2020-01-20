92a897c839efa75e0041b572b2be411168ec5403
dmgrinko1@gmail.com <dmgrinko1@gmail.com>
2020-01-20 16:35:41 +0200

    #cosmetic

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -3,19 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe AuthorizeApiRequest do
-  # Create test user
   let(:user) { create(:user) }
-  # Mock `Authorization` header
   let(:header) { { 'Authorization' => token_generator(user.id) } }
-  # Invalid request subject
+
   subject(:invalid_request_obj) { described_class.new({}) }
-  # Valid request subject
   subject(:request_obj) { described_class.new(header) }
 
-  # Test Suite for AuthorizeApiRequest#call
-  # This is our entry point into the service class
   describe '#call' do
-    # returns user object when request is valid
     context 'when valid request' do
       it 'returns user object' do
         result = request_obj.call
@@ -23,7 +17,6 @@ RSpec.describe AuthorizeApiRequest do
       end
     end
 
-    # returns error message when invalid request
     context 'when invalid request' do
       context 'when missing token' do
         it 'raises a MissingToken error' do
